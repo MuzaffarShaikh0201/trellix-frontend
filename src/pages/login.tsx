96bb1139fb79a482918dee44ee4bc800a61c777b
MuzaffarShaikh0201 <114210961+MuzaffarShaikh0201@users.noreply.github.com>
2025-07-23 16:35:43 +0530

    Added header and fotter in login page for md devices

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -53,9 +53,15 @@ const Login = () => {
 					</div>
 				</div>
 			</div>
-			<div className="bg-background-secondary h-full w-full md:w-[30%] flex justify-center items-center p-4">
-				<div className="flex flex-col justify-center items-center w-full p-4">
-					<div className="flex flex-col justify-center items-start w-full p-4 mb-2">
+			<div className="bg-background-secondary h-full w-full md:w-[30%] flex flex-col md:justify-center justify-between items-center p-4">
+				<div className="md:hidden flex justify-start items-center w-full p-2 gap-2">
+					<img src="/layers.svg" alt="Logo" className="w-8 h-8" />
+					<h1 className="text-2xl font-bold text-text-primary">
+						Trellix
+					</h1>
+				</div>
+				<div className="flex flex-col justify-center items-center w-full md:p-4 p-2">
+					<div className="flex flex-col justify-center items-start w-full md:p-4 p-2 mb-2">
 						<h1 className="text-3xl font-bold text-text-primary">
 							Login
 						</h1>
@@ -63,7 +69,7 @@ const Login = () => {
 							Welcome back! Please login to your account.
 						</p>
 					</div>
-					<div className="w-full flex flex-col justify-center items-start p-4 gap-4 mt-4">
+					<div className="w-full flex flex-col justify-center items-start md:p-4 p-2 gap-4 mt-4">
 						<FormField
 							title="Email"
 							placeholder="Enter your email..."
@@ -83,7 +89,7 @@ const Login = () => {
 							) => setPassword(e.target.value)}
 						/>
 					</div>
-					<div className="w-full flex justify-end items-center p-4">
+					<div className="w-full flex justify-end items-center md:p-4 p-2">
 						<a
 							href="#"
 							className="text-sm text-primary hover:underline"
@@ -91,7 +97,7 @@ const Login = () => {
 							forgot password?
 						</a>
 					</div>
-					<div className="w-full flex flex-col gap-4 justify-center items-center p-4">
+					<div className="w-full flex flex-col gap-4 justify-center items-center md:p-4 p-2">
 						<Button
 							title="Login"
 							fill={true}
@@ -106,7 +112,7 @@ const Login = () => {
 							disabled={false}
 						/>
 					</div>
-					<div className="w-full flex justify-center items-center p-4">
+					<div className="w-full flex justify-center items-center md:p-4 p-2">
 						<p className="text-sm text-text-secondary">
 							Don't have an account?{" "}
 							<a
@@ -117,6 +123,17 @@ const Login = () => {
 							</a>
 						</p>
 					</div>
+				</div>
+				<div className="self-end w-full md:hidden flex flex-col justify-center items-center p-2">
+					<p className="text-sm text-text-secondary">
+						&copy; Trellix 2025
+					</p>
+					<a
+						href="#"
+						className="text-sm text-text-secondary hover:underline"
+					>
+						Terms of use & Privacy Policy
+					</a>
 				</div>
 			</div>
 		</div>
