470c82183f28a3480b53c640284a47b9bdef0ebd
MuzaffarShaikh0201 <muzaffarshaikh0201@gmail.com>
2025-08-28 09:48:14 +0530

    Add axios and history dependencies, implement custom loading state in ProtectedRoute, and restore ThemeContext and Dashboard components

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -2,16 +2,22 @@ import React from "react";
 import { Navigate, Outlet } from "react-router";
 import { useAuth } from "../contexts/AuthContext";
 import { history } from "../services/history";
+import CustomLoader from "./CustomLoader";
 
 const ProtectedRoute: React.FC = () => {
 	const { isAuthenticated, loading } = useAuth();
 
 	if (loading) {
-		return <div>Loading...</div>;
+		return (
+			<div className="w-screen h-screen bg-background-primary">
+				<CustomLoader />
+			</div>
+		);
 	}
 
 	if (!isAuthenticated) {
 		history.replace("/login", { from: history.location });
+		Navigate({ to: "/login" });
 		return null;
 	}
 
