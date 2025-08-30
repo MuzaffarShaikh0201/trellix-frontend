e1bb64b9d0e5215e96782891a68d4fc7bd09dcaf
MuzaffarShaikh0201 <muzaffarshaikh0201@gmail.com>
2025-08-30 10:55:58 +0530

    Refactored

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,19 @@
 import CustomLoader from "../components/CustomLoader";
+import { useAuth } from "../contexts/AuthContext";
 
 const Dashboard = () => {
+	const { user, loading } = useAuth();
+
+	if (loading) {
+		return (
+			<div className="w-screen h-screen flex justify-center items-center">
+				<CustomLoader size={36} />
+			</div>
+		);
+	}
 	return (
-		<div className="w-full h-full flex justify-center items-center">
-			<CustomLoader size={80} />
+		<div className="w-screen h-screen flex justify-center items-center">
+			{user.first_name} {user.last_name}
 		</div>
 	);
 };
